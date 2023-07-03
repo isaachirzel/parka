b1fd22e4641abc7dbc91a0b4caeafa6b7e27ed38
Ike Hirzel <hirzelisaac@gmail.com>
2023-07-03 08:24:01 -0400

    File cleanup

--- include/parka/ast/Module.hpp
+++ include/parka/ast/Module.hpp
@@ -20,15 +20,17 @@ namespace parka::ast
 
 	public:
 
-		ModuleAst(String&& filepath, Array<FunctionAst*>&& functions, Array<StructAst*>&& structs);
+		ModuleAst(String&& filepath, Array<FunctionAst*>&& functions, Array<StructAst*>&& structs):
+		_filepath(std::move(filepath)),
+		_functions(std::move(functions)),
+		_structs(std::move(structs))
+		{}
 		ModuleAst(ModuleAst&&) = default;
 		ModuleAst(const ModuleAst&) = delete;
 
 		const auto& filename() const { return _filepath; }
 		const auto& functions() const { return _functions; }
 		const auto& structs() const { return _structs; }
-
-		friend std::ostream& operator<<(std::ostream& out, const ModuleAst& mod);
 	};
 }
 
