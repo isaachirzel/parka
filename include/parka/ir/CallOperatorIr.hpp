734359cb90fbaf716f7868b74ffd2ca037b497f1
Isaac Hirzel <hirzelisaac@gmail.com>
2024-05-25 10:20:38 -0400

    Add call operatorir

---
+++ include/parka/ir/CallOperatorIr.hpp
@@ -0,0 +1,25 @@
+#ifndef PARKA_IR_CALL_OPERATOR_IR_HPP
+#define PARKA_IR_CALL_OPERATOR_IR_HPP
+
+namespace parka::ir
+{
+	class CallOperatorIr
+	{
+		TypeIr _returnType;
+		Array<TypeIr> _parameterTypes;
+
+	public:
+
+		CallOperatorIr(TypeIr&& returnType, Array<TypeIr>&& parameterTypes):
+			_returnType(std::move(returnType)),
+			_parameterTypes(std::move(parameterTypes))
+		{}
+		CallOperatorIr(CallOperatorIr&&) = default;
+		CallOperatorIr(const CallOperatorIr&) = delete;
+
+		const auto& returnType() const { return _returnType; }
+		const auto& parameterTypes() const { return _parameterTypes; }
+	};
+}
+
+#endif
